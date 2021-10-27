a8181f604d8535ce05a44e043e97abbb7ea6af85
stojl <lucashillerod@hotmail.com>
2021-10-27 22:58:38 +0200

    Simplified simulation function.

--- School/Simulation_af_Snyd/Snyd_simulation.R
+++ School/Simulation_af_Snyd/Snyd_simulation.R
@@ -1,24 +1,21 @@
-library(tibble)
-library(magrittr)
 library(Rcpp)
 sourceCpp(file = "School/Simulation_af_Snyd/dcount.cpp")
 
-rollbeforecomp <- function(dice_vector, m){
+rollbeforecomp <- function(dice_vector, m = 1){
   
   #Initialize
   total_dice <- sum(dice_vector)
   result <- c()
-  
+
   #Simulation
   for(i in 1:m){
-    result <- sample(1:6, total_dice, replace = TRUE) %>% 
-      sumDice(dice_vector) %>% c(result, .)
+    result <- c(result, sumDice(sample(1:6, total_dice, replace = TRUE), dice_vector))
   }
   
-  result %>% tibble(gameid = rep(1:m, each = 6), dice_value = rep(1:6, m), dice_count = .)
-  
+  #Output
+  m <- matrix(c(rep(1:m, each = 6), rep(1:6, m), result), m*6, 3)
+  colnames(m) <- c("gameid", "value", "count")
+  return(m)
 }
 
 simulate_roll <- compiler::cmpfun(rollbeforecomp)
-
-
