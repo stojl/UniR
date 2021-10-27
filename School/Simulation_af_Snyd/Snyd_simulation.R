656301453d9df8c05525980af60a16a927331280
stojl <lucashillerod@hotmail.com>
2021-10-27 23:13:04 +0200

    Further optimization.

--- School/Simulation_af_Snyd/Snyd_simulation.R
+++ School/Simulation_af_Snyd/Snyd_simulation.R
@@ -4,16 +4,18 @@ sourceCpp(file = "School/Simulation_af_Snyd/dcount.cpp")
 rollbeforecomp <- function(dice_vector, m = 1){
   
   #Initialize
+  total_length <- 6 * m
+  onesix <- 1:6
   total_dice <- sum(dice_vector)
-  result <- c()
+  result <- vector(mode = "double", length = total_length)
 
   #Simulation
   for(i in 1:m){
-    result <- c(result, sumDice(sample(1:6, total_dice, replace = TRUE), dice_vector))
+    result[((i-1)*6 + 1):(i*6)] <- sumDice(sample(onesix, total_dice, replace = TRUE), dice_vector)
   }
   
   #Output
-  m <- matrix(c(rep(1:m, each = 6), rep(1:6, m), result), m*6, 3)
+  m <- matrix(c(rep(1:m, each = 6), rep(onesix, m), result), total_length, 3)
   colnames(m) <- c("gameid", "value", "count")
   return(m)
 }
